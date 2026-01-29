72fcbdc00fb2459105e3c453ad0dedd4c80d1e2c
Ravuri Greeshmanth sai venkatesh <greeshmanth23@gmail.com>
2026-01-29 22:15:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function playsound(event){
+    switch(event.key){
+        case "q":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "w":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "e":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "r":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "i":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "o":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "p":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default:
+            console.log(event.key);
+    }
+}
+document.addEventListener("keypress", playsound);
